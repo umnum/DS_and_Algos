b1626244466b0a7538f67e8cc9b468b49ee16a98
Michael Castanieto <mcastanieto@gmail.com>
2021-02-08 16:32:34 -0800

    create solution template for Binary Tree Diameter

---
+++ Binary_Tree_Diameter/solution1.js
@@ -0,0 +1,17 @@
+// This is an input class. Do not edit.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function binaryTreeDiameter(tree) {
+  // Write your code here.
+  return -1;
+}
+
+// Do not edit the line below.
+exports.binaryTreeDiameter = binaryTreeDiameter;
+exports.BinaryTree = BinaryTree;
