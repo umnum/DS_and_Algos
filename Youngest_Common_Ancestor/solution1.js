82b9c4511aad39ad1fdd720b2d99a0f87e78e150
Michael Castanieto <mcastanieto@gmail.com>
2021-03-09 14:30:01 -0800

    fix Youngest Common Ancestor

--- Youngest_Common_Ancestor/solution1.js
+++ Youngest_Common_Ancestor/solution1.js
@@ -1,19 +1,21 @@
 function getYoungestCommonAncestor(topAncestor, descendantOne, descendantTwo) {
   // Write your code here.
-	return
-	let visited = {};
-	visited[descendantOne.name] = true;
-	visited[descendantTwo.name] = true;
-	if (visited[topAncestor.name]) return topAncestor.name;
-	let node1 = descendantOne;
-	let node2 = descendantTwo;
-	while (node1.ancestor && !visited[node1.ancestor.name]) {
-		node1 = node1.ancestor;
-		visited[node1.name] = true;
-	}
-	while (node2.ancestor && !visited[node2.ancestor.name]) {
-		node2 = node2.ancestor;
-		visited[node2.name] = true;
-	}
-	return {"nodeId": node2.ancestor.name};
+  let visited = {};
+  visited[descendantOne.name] = true;
+  visited[descendantTwo.name] = true;
+  if (visited[topAncestor.name]) return topAncestor;
+  let node1 = descendantOne;
+  let node2 = descendantTwo;
+  while (node1.ancestor && !visited[node1.ancestor.name]) {
+  node1 = node1.ancestor;
+    visited[node1.name] = true;
+  }
+  if (node1.ancestor && node1.ancestor.name === descendantTwo.name) {
+    return node1.ancestor;
+  }
+  while (node2.ancestor && !visited[node2.ancestor.name]) {
+    node2 = node2.ancestor;
+    visited[node2.name] = true;
+  }
+  return node2.ancestor;
 }
