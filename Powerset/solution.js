e52199aeae47d68aee11e500c20894b14f03db65
Michael Castanieto <mcastanieto@gmail.com>
2021-04-01 17:34:57 -0700

    revise powerset solution

--- Powerset/solution.js
+++ Powerset/solution.js
@@ -1,10 +1,10 @@
 function powerset(array) {
   // Write your code here.
 	const subsets = [[]];
-	for (let i = 0; i < array.length; i++) {
+	for (const ele of array ){
 		const length = subsets.length;
-		for (let j = 0; j < length; j++) {
-			subsets.push(subsets[j].concat(array[i]));
+		for (let i = 0; j < length; i++) {
+			subsets.push(subsets[j].concat(ele));
 		}
 	}
 	return subsets;
